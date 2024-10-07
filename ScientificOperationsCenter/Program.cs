aa38315f6046bed8990f51e1bdfe041168e7f906
aleixbossa <aleixbossa@gmail.com>
2024-10-07 15:20:38 -0400

    added connection to db
    still collection to be added

--- ScientificOperationsCenter/Program.cs
+++ ScientificOperationsCenter/Program.cs
@@ -1,4 +1,26 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//will have to move it somewhere else probably
+const string connectionUri = "mongodb+srv://scyops:1234321@scientificopscentre.g5xsk.mongodb.net/?retryWrites=true&w=majority&appName=ScientificOpsCentre";
+
+    var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+    // Set the ServerApi field of the settings object to set the version of the Stable API on the client
+    settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+    // Create a new client and connect to the server
+    var client = new MongoClient(settings);
+    // Send a ping to confirm a successful connection
+    try {
+        var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+        Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (Exception ex) {
+    Console.WriteLine(ex);
+    }
 
 // Add services to the container.
 builder.Services.AddRazorPages();
