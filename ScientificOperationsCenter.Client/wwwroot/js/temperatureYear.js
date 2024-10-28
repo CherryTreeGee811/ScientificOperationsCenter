de69b26bdfb88fa7d792dac08b0aaa2f5e85419f
CherryTreeGee811 <79172404+CherryTreeGee811@users.noreply.github.com>
2024-10-27 21:01:13 -0400

    Downgrade to http - JT

--- ScientificOperationsCenter.Client/wwwroot/js/temperatureYear.js
+++ ScientificOperationsCenter.Client/wwwroot/js/temperatureYear.js
@@ -3,7 +3,7 @@
 
 function getChartData() {
     const date = '2024-10-08';
-    const url = `https://localhost:5001/api/Temperatures/year?date=${date}`;
+    const url = `http://localhost:5000/api/Temperatures/year?date=${date}`;
     const errorTextElement = document.getElementById("ErrorText");
     const loadingTextElement = document.getElementById("LoadingText");
     loadingTextElement.textContent = "Loading Chart ...";
