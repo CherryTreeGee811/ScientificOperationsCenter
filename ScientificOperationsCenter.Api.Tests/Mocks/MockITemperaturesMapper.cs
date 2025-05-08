7f5fdebf1311ea9bf11f94518a08588764a9a6d3
CherryTreeGee811 <79172404+CherryTreeGee811@users.noreply.github.com>
2025-05-08 13:32:01 -0400

    Simplify code further
    Remove unnecessary type declaration for DateOnly and TimeOnly. Types are already declared for each of the fields in the models.

--- ScientificOperationsCenter.Api.Tests/Mocks/MockITemperaturesMapper.cs
+++ ScientificOperationsCenter.Api.Tests/Mocks/MockITemperaturesMapper.cs
@@ -15,37 +15,37 @@ namespace ScientificOperationsCenter.Api.Tests.Mocks
 
             var temperaturesSameDay = new List<TemperaturesTimeAverage>()
             {
-                new() { Time = new TimeOnly(01, 00), AverageTemperature = -4 },
-                new() { Time = new TimeOnly(21, 00), AverageTemperature = 30 },
-                new() { Time = new TimeOnly(06, 00), AverageTemperature = 12 },
-                new() { Time = new TimeOnly(03, 00), AverageTemperature = -2 },
-                new() { Time = new TimeOnly(04, 00), AverageTemperature = 23 },
-                new() { Time = new TimeOnly(02, 00), AverageTemperature = 0 },
+                new() { Time = new (01, 00), AverageTemperature = -4 },
+                new() { Time = new (21, 00), AverageTemperature = 30 },
+                new() { Time = new (06, 00), AverageTemperature = 12 },
+                new() { Time = new (03, 00), AverageTemperature = -2 },
+                new() { Time = new (04, 00), AverageTemperature = 23 },
+                new() { Time = new (02, 00), AverageTemperature = 0 },
             };
 
 
             var temperaturesSameMonth = new List<TemperaturesDateAverage>()
             {
-                new() { Date = new DateOnly(2024, 10, 09), AverageTemperature = -1 },
-                new() { Date = new DateOnly(2024, 10, 02), AverageTemperature = 33 },
-                new() { Date = new DateOnly(2024, 10, 11), AverageTemperature = 12 },
-                new() { Date = new DateOnly(2024, 10, 20), AverageTemperature = 1 },
-                new() { Date = new DateOnly(2024, 10, 21), AverageTemperature = 20 },
-                new() { Date = new DateOnly(2024, 10, 06), AverageTemperature = 4 },
-                new() { Date = new DateOnly(2024, 10, 01), AverageTemperature = 32 },
+                new() { Date = new (2024, 10, 09), AverageTemperature = -1 },
+                new() { Date = new (2024, 10, 02), AverageTemperature = 33 },
+                new() { Date = new (2024, 10, 11), AverageTemperature = 12 },
+                new() { Date = new (2024, 10, 20), AverageTemperature = 1 },
+                new() { Date = new (2024, 10, 21), AverageTemperature = 20 },
+                new() { Date = new (2024, 10, 06), AverageTemperature = 4 },
+                new() { Date = new (2024, 10, 01), AverageTemperature = 32 },
             };
 
 
             var temperaturesSameYear = new List<TemperaturesDateAverage>()
             {
-                new() { Date = new DateOnly(2025, 10, 01), AverageTemperature = 20 },
-                new() { Date = new DateOnly(2025, 06, 01), AverageTemperature = 0 },
-                new() { Date = new DateOnly(2025, 07, 01), AverageTemperature = 6 },
-                new() { Date = new DateOnly(2025, 12, 01), AverageTemperature = 20 },
-                new() { Date = new DateOnly(2025, 08, 01), AverageTemperature = 15 },
-                new() { Date = new DateOnly(2025, 09, 01), AverageTemperature = 19 },
-                new() { Date = new DateOnly(2025, 11, 01), AverageTemperature = 12 },
-                new() { Date = new DateOnly(2025, 05, 01), AverageTemperature = -2 },
+                new() { Date = new (2025, 10, 01), AverageTemperature = 20 },
+                new() { Date = new (2025, 06, 01), AverageTemperature = 0 },
+                new() { Date = new (2025, 07, 01), AverageTemperature = 6 },
+                new() { Date = new (2025, 12, 01), AverageTemperature = 20 },
+                new() { Date = new (2025, 08, 01), AverageTemperature = 15 },
+                new() { Date = new (2025, 09, 01), AverageTemperature = 19 },
+                new() { Date = new (2025, 11, 01), AverageTemperature = 12 },
+                new() { Date = new (2025, 05, 01), AverageTemperature = -2 },
             };
 
 
--- ScientificOperationsCenter.Api.Tests/Mocks/MockITemperaturesRepository.cs
+++ ScientificOperationsCenter.Api.Tests/Mocks/MockITemperaturesRepository.cs
@@ -14,18 +14,18 @@ namespace ScientificOperationsCenter.Api.Tests.Mocks
 
             var temperatures = new List<Temperatures>()
             {
-                new() { Id = 1, Date = new DateOnly(2024, 10, 08), Time = new TimeOnly(16, 00), TemperatureCelcius = -4 },
-                new() { Id = 2, Date = new DateOnly(2024, 10, 08), Time = new TimeOnly(19, 00), TemperatureCelcius = 12 },
-                new() { Id = 3, Date = new DateOnly(2024, 10, 08), Time = new TimeOnly(19, 20), TemperatureCelcius = 22 },
-                new() { Id = 4, Date = new DateOnly(2024, 10, 08), Time = new TimeOnly(12, 00), TemperatureCelcius = 8 },
-                new() { Id = 5, Date = new DateOnly(2024, 11, 03), Time = new TimeOnly(04, 30), TemperatureCelcius = 5 },
-                new() { Id = 6, Date = new DateOnly(2024, 10, 09), Time = new TimeOnly(21, 30), TemperatureCelcius = 12 },
-                new() { Id = 7, Date = new DateOnly(2024, 10, 08), Time = new TimeOnly(12, 40), TemperatureCelcius = 10 },
-                new() { Id = 8, Date = new DateOnly(2025, 01, 03), Time = new TimeOnly(05, 30), TemperatureCelcius = 15 },
-                new() { Id = 9, Date = new DateOnly(2024, 10, 09), Time = new TimeOnly(21, 00), TemperatureCelcius = 10 },
-                new() { Id = 10, Date = new DateOnly(2024, 10, 09), Time = new TimeOnly(21, 31), TemperatureCelcius = 9 },
-                new() { Id = 11, Date = new DateOnly(2024, 11, 02), Time = new TimeOnly(09, 10), TemperatureCelcius = -2 },
-                new() { Id = 12, Date = new DateOnly(2025, 01, 05), Time = new TimeOnly(03, 30), TemperatureCelcius = 5 }
+                new() { Id = 1, Date = new (2024, 10, 08), Time = new (16, 00), TemperatureCelcius = -4 },
+                new() { Id = 2, Date = new (2024, 10, 08), Time = new (19, 00), TemperatureCelcius = 12 },
+                new() { Id = 3, Date = new (2024, 10, 08), Time = new (19, 20), TemperatureCelcius = 22 },
+                new() { Id = 4, Date = new (2024, 10, 08), Time = new (12, 00), TemperatureCelcius = 8 },
+                new() { Id = 5, Date = new (2024, 11, 03), Time = new (04, 30), TemperatureCelcius = 5 },
+                new() { Id = 6, Date = new (2024, 10, 09), Time = new (21, 30), TemperatureCelcius = 12 },
+                new() { Id = 7, Date = new (2024, 10, 08), Time = new (12, 40), TemperatureCelcius = 10 },
+                new() { Id = 8, Date = new (2025, 01, 03), Time = new (05, 30), TemperatureCelcius = 15 },
+                new() { Id = 9, Date = new (2024, 10, 09), Time = new (21, 00), TemperatureCelcius = 10 },
+                new() { Id = 10, Date = new (2024, 10, 09), Time = new (21, 31), TemperatureCelcius = 9 },
+                new() { Id = 11, Date = new (2024, 11, 02), Time = new (09, 10), TemperatureCelcius = -2 },
+                new() { Id = 12, Date = new (2025, 01, 05), Time = new (03, 30), TemperatureCelcius = 5 }
             };
 
 
